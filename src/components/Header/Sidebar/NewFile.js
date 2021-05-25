a92c44259882287ec8f478fb8cb8003882b5a697
Zach Lynch <zachary.lynch1337@gmail.com>
2021-05-25 16:20:18 -0700

    Created side bar and new file containers

---
+++ src/components/Header/Sidebar/NewFile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NewFile = () => {
+    return (
+        <div className="newFile">
+            <div className="newFile_container">
+                {/* + icon*/}
+                <p>New File</p>
+            </div>
+        </div>
+    )
+}
+
+export default NewFile
---
+++ src/components/Header/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            {/* New File*/}
+
+            <div className="sidebar_container">
+                {/* Side Bar Icons*/}
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
