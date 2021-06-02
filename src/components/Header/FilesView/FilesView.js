ec21e5d45ac39b69e3da6a7455d3d2f537cb8ff7
Zach Lynch <zachary.lynch1337@gmail.com>
2021-06-01 17:48:47 -0700

    created File view container

---
+++ src/components/Header/FilesView/FilesView.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FilesView = () => {
+    return (
+        <div className="fileView">
+            <div className="fileView-row">
+                {/* cards */}
+            </div>
+            <div className="fileView-tiles">
+                <div className="fileView-tiles-left">
+                    <p>name</p>
+                </div>
+                <div className="fileView-tiles-right">
+                    <p>Last modified</p>
+                    <p>File size</p>
+                </div>
+            </div>
+            {/* file items */}
+        </div>
+    )
+}
+
+export default FilesView
