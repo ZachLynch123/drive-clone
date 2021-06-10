90068dae0ccf066fb861fad873bbe69e048bb61a
Zach Lynch <zachary.lynch1337@gmail.com>
2021-06-10 11:05:40 -0700

    Fixed firebase authentication issue

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
   const auth = firebase.auth()
-  const provider = new firebase.auth.GoogleProvider();
+  const provider = new firebase.auth.GoogleAuthProvider();
   const storage = firebase.storage()
   const db = firebaseApp.firestore();
 
