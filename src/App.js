ba040e138ab9bcbbd3b94349ec013b60c60f2a12
Zach Lynch <zachary.lynch1337@gmail.com>
2021-07-08 23:10:23 -0700

    commented code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { auth, provider } from './firebase';
 
 function App() {
   // auth
+  // remembers user on refresh
   const [user, setUser] = useState();
   auth.onAuthStateChanged(setUser)
 
